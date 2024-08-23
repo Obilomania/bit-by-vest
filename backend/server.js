bf12b9d4b4e5fff6f173464164ba72c62e789c43
Obilomania <iloanyaobinna@gmail.com>
2024-08-23 02:17:48 +0100

    change small

--- backend/server.js
+++ backend/server.js
@@ -42,11 +42,11 @@ app.use("/api/user-section", userRoute);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, "..", "frontend", "build", "index.html")
+      path.resolve(__dirname, "frontend", "build", "index.html")
     );
   });
 }
